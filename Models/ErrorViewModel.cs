ef2c2422993746ad32a36dee5b1aba6ef2f4c7ee
SHIRLY JUSID <46875521@ort>
2022-06-30 14:24:40 -0300

    Create .NET MVC Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Sala_Escape_Harry.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
